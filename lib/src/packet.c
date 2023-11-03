f86838d052b7be5bd05612904e1ddef32fb5a0fb
yaxiong <xieyaxiongfly@gmail.com>
2023-11-03 03:31:04 -0400

    main

--- lib/src/packet.c
+++ lib/src/packet.c
@@ -36,7 +36,7 @@ int packet_decompose(char pkt_buf[1500], int pkt_size, pkt_header_t *pkt_header,
   int head_size = sizeof(pkt_header_t);
   memcpy(pkt_header, pkt_buf, head_size);
 
-  memcpy(pkt_buf[head_size], payload_buf, pkt_size - head_size);
+  memcpy(&pkt_buf[head_size], payload_buf, pkt_size - head_size);
 
   return 0;
 }
