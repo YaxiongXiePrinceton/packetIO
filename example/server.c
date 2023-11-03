7872c2b910afb21392f3fb8f7c4b8ba4de657778
yaxiong <xieyaxiongfly@gmail.com>
2023-11-03 03:45:33 -0400

    main

--- example/server.c
+++ example/server.c
@@ -91,7 +91,7 @@ int main(int argc, char **argv) {
   serv_cli_config_t new_config;
   memcpy(&new_config, payload_buf, sizeof(serv_cli_config_t));
 
-  printf("config knownIP:%d local IP:%s separateTXRX:%d\n", new_config.known_IP,
+  printf("config knownIP:%d local IP:%s separateTXRX:%d\n", new_config.IP_known,
          new_config.local_IP, new_config.separate_txrx);
 
   if (config.sender) {
